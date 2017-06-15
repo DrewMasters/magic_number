832bfbd1c9b594594bd9e8553c88ed1ed876fb0c
Drew Masters <dmaster1@wiseacre.eecs.utk.edu>
2017-06-15 11:27:08 -0400

    deleted test files and finished code to parse espn fantasy football scoreboard

--- get_scores.py
+++ get_scores.py
@@ -7,8 +7,15 @@ page = urllib2.urlopen(url)
 
 soup = BeautifulSoup(page)
 
-scores = soup.find('div', id='scoreboardMatchups').findAll('table')
+scores = soup.find('div', id='scoreboardMatchups').findAll('table',{"class" : "ptsBased matchup"})
 
-#scores = soup.findAll('table')
 
-print scores
+for game in scores:
+	teams = game.findAll('tr')
+	for t in teams:
+		team = t.find('a')
+		name = t.find('span',{"class" : "owners"})
+		score = t.find('td',{"class" : "score"})
+		if team != None:
+			print str(team.contents[0].encode("ascii")) + " " + str(name.contents[0].encode("ascii")) + " " + str(score.contents[0].encode("ascii"))
+	print
